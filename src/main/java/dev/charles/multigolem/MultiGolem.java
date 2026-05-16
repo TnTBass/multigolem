d66ed16c111a5711a4b0c3dc615904c09f9f9b88
TnTBass <tyler.jurgens@gmail.com>
2026-05-15 19:43:19 -0600

    chore: bootstrap from Fabric 26.1.2 example mod

---
+++ src/main/java/dev/charles/multigolem/MultiGolem.java
@@ -0,0 +1,15 @@
+package dev.charles.multigolem;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MultiGolem implements ModInitializer {
+    public static final String MOD_ID = "multigolem";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOG.info("MultiGolem starting up - Charles & Tyler's golem variants");
+    }
+}
